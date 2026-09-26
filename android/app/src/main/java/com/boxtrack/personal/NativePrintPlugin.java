6139fddfbab9c1c1da19c872bda900a7db0a3ffd
Luis Felipe Alves <luisfelipe.alves@gmail.com>
2026-09-26 12:25:01 +0000

    fix: correct Capacitor PluginMethod import

--- android/app/src/main/java/com/boxtrack/personal/NativePrintPlugin.java
+++ android/app/src/main/java/com/boxtrack/personal/NativePrintPlugin.java
@@ -10,8 +10,8 @@ import android.webkit.WebView;
 
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
 
 @CapacitorPlugin(name = "NativePrint")
 public class NativePrintPlugin extends Plugin {
